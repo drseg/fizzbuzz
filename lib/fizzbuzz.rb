81f9426561df53498654bdb8dc91b5ff48362c21
Daniel Segall <segalld@gmail.com>
2019-07-24 17:09:25 +0100

    Seventh test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz(n)
   return "Fizz" if n % 3 == 0
-  return "Buzz" if n == 5
+  return "Buzz" if n % 5 == 0
   return n
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,34 +4,28 @@ describe 'fizzbuzz' do
   it 'returns 1 when passed 1' do
     expect(fizzbuzz(1)).to eq 1
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns 2 when passed 2' do
     expect(fizzbuzz(2)).to eq 2
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns "Fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq "Fizz"
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns 4 when passed 4' do
     expect(fizzbuzz(4)).to eq 4
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns "Buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq "Buzz"
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns "Fizz" when passed 6' do
     expect(fizzbuzz(6)).to eq "Fizz"
+  end
+
+  it 'returns "Buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq "Buzz"
   end
 end
