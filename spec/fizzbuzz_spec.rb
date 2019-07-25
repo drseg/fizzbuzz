c80d4b9d6ae66c1aafa9e523d992af20e6e6e5ee
Daniel Segall <segalld@gmail.com>
2019-07-25 16:23:58 +0100

    Move fizzbuzz calls into test helper method

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,35 +3,39 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+  def fizzbuzz(num)
+    num.fizzbuzz
+  end
+
   it 'returns 1 when passed 1' do
-    expect(1.fizzbuzz).to eq '1'
+    expect(fizzbuzz(1)).to eq '1'
   end
 
   it 'returns 2 when passed 2' do
-    expect(2.fizzbuzz).to eq '2'
+    expect(fizzbuzz(2)).to eq '2'
   end
 
   it 'returns "Fizz" when passed 3' do
-    expect(3.fizzbuzz).to eq 'Fizz'
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end
 
   it 'returns 4 when passed 4' do
-    expect(4.fizzbuzz).to eq '4'
+    expect(fizzbuzz(4)).to eq '4'
   end
 
   it 'returns "Buzz" when passed 5' do
-    expect(5.fizzbuzz).to eq 'Buzz'
+    expect(fizzbuzz(5)).to eq 'Buzz'
   end
 
   it 'returns "Fizz" when passed 6' do
-    expect(6.fizzbuzz).to eq 'Fizz'
+    expect(fizzbuzz(6)).to eq 'Fizz'
   end
 
   it 'returns "Buzz" when passed 10' do
-    expect(10.fizzbuzz).to eq 'Buzz'
+    expect(fizzbuzz(10)).to eq 'Buzz'
   end
 
   it 'returns "FizzBuzz" when passed 15' do
-    expect(15.fizzbuzz).to eq 'FizzBuzz'
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
   end
 end
